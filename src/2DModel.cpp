e1fdba870b2fa5fda89640f2653758fe20847c56
branyoto-etud <bgamei01@etud.u-pem.fr>
2022-01-08 23:03:32 +0100

    Raw version

--- src/2DModel.cpp
+++ src/2DModel.cpp
@@ -1,120 +1,417 @@
-#include <FluidGrid2.hpp>
-
-#include <iostream>
-
-#include "2DGraphics.hpp"
+#include <cstdlib>
+#include <cstdio>
 #include <vector>
-#include <iterator>
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
+#include "SDLWindowManager.hpp"
+#include <GL/glew.h>
+#include <iostream>
+#include "Program.hpp"
+#include "FilePath.hpp"
 
-std::vector<float> flat(std::vector<std::vector<double>> grid){
-        std::vector<float> acc;
-        using std::begin;
-        using std::end;
-        for (auto vect : grid){
-            acc.insert(end(acc), begin(vect), end(vect));
-        }
-        return acc;
-    }
+/* macros */
 
-int main(int argc, char* argv[])
+#define IX(i, j) ((i)+(N+2)*(j))
+
+/* external definitions (from solver.c) */
+
+extern void dens_step(int N, float *x, float *x0, float *u, float *v, float diff, float dt);
+
+extern void vel_step(int N, float *u, float *v, float *u0, float *v0, float viscosity, float dt);
+
+/* global variables */
+
+// Config
+static int N;
+static int Ts; // Tile size
+static float Nf;
+static float dt, diff, visc;
+static float force, source;
+
+// Data
+static float *u, *v, *u_prev, *v_prev;
+static float *dens, *dens_prev;
+
+// Event
+static int win_x, win_y;
+static int mouse_down[3];
+static int omx, omy, mx, my;
+
+
+/*
+  ----------------------------------------------------------------------
+   free/clear/allocate simulation data
+  ----------------------------------------------------------------------
+*/
+
+
+static void free_data()
 {
-    using namespace std;
-    int size = 9;
-
-    FluidGrid2 grid { size, .01, 0.1 };
-    
-    cout << grid << endl;
-    grid.add_density (Tuple<int> (2,1), 100.);
-    grid.add_density (Tuple<int> (3,5), 1.);
-    grid.add_density (Tuple<int> (8,8), 100.);
-
-    cout << grid._sources;
-    Grid vx = GridStruct(size).init();
-    Grid vy = GridStruct(size).init();
-    for (int i = 0; i < size; i++) 
+    if (u)
     {
-        for (int j = 0; j < size; j++)
-        {
-            vx[i][j] = 1.;
-        }
+        free(u);
     }
-    //cout << vy << vx;
-    grid.dens_step(Tuple<Grid> (vx, vy));
-    cout << grid._densities;
-    grid.dens_step(Tuple<Grid> (vx, vy));
-    cout << grid._densities;
-    
-    
-
-    /*std::vector<float> rid = 
-    {1.,0.,0.,0.,0.,1.,
-     0.,1.,0.,0.,0.,0.,
-     0.,0.5,1.,0.,0.,0.05,
-     0.,1.,0.,1.,0.,0.,
-     0.,0.,0.,0.,1.,0.,
-     0.,0.,0.,0.,0.,1.};*/
-
-     std::vector<float> rid = 
-    {1., 0., 0., 0., 0., 1., 0., 0., 0.,
-     0., 1., 0., 0., 0., 0., 0., 0., 0.,
-     0., 0.5, 1., 0., 0., 0.05, 0., 0., 0.,
-     0., 1., 0., 1., 0., 0., 0., 0., 0.,
-     0., 0., 0., 0., 1., 0., 0., 0., 0.,
-     0., 0., 0., 0., 0., 1., 0., 0., 0., 
-     0., 0., 0., 0., 0., 0., 1., 0., 0.,
-     0., 0., 0., 0., 0., 0., 0., 1., 0.,
-     0., 0., 0., 0., 0., 0., 0., 0., 1.};
-    
-    GridWindowManager window(500,500, size, size);
-    bool done = false;
-    while(!done)
+    if (v)
     {
-        SDL_Event e;
-        while(SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_QUIT)
-            {
-                done = true;
-            }
-        }
-        window.display_grid<float>( flat(grid._sources), [](float i){ return Color(0,0,i);});
-        //sleep(2);
-        grid.dens_step(Tuple<Grid> (vx, vy));
-        /*window.display_grid<float>( rid, [](float i){ return Color(0,0,i);});
-        sleep(2);*/
-        
+        free(v);
     }
-    return 0;
+    if (u_prev)
+    {
+        free(u_prev);
+    }
+    if (v_prev)
+    {
+        free(v_prev);
+    }
+    if (dens)
+    {
+        free(dens);
+    }
+    if (dens_prev)
+    {
+        free(dens_prev);
+    }
+}
+
+static void clear_data()
+{
+    int i, size = (N + 2) * (N + 2);
+
+    for (i = 0; i < size; i++)
+    {
+        u[i] = v[i] = u_prev[i] = v_prev[i] = dens[i] = dens_prev[i] = 0.0f;
+    }
+}
+
+static int allocate_data()
+{
+    int size = (N + 2) * (N + 2);
+
+    u = (float *) malloc(size * sizeof(float));
+    v = (float *) malloc(size * sizeof(float));
+    u_prev = (float *) malloc(size * sizeof(float));
+    v_prev = (float *) malloc(size * sizeof(float));
+    dens = (float *) malloc(size * sizeof(float));
+    dens_prev = (float *) malloc(size * sizeof(float));
+
+    if (!u || !v || !u_prev || !v_prev || !dens || !dens_prev)
+    {
+        fprintf(stderr, "cannot allocate data\n");
+        return (0);
+    }
+
+    return (1);
 }
 
 
+/*
+  ----------------------------------------------------------------------
+   OpenGL specific drawing routines
+  ----------------------------------------------------------------------
+*/
+struct Color
+{
+    float r, g, b;
+};
 
 
+float min(float a, float b)
+{
+    return a < b ? a : b;
+}
 
+float max(float a, float b)
+{
+    return a > b ? a : b;
+}
+
+Color lerp(Color a, Color b, float t)
+{
+    return {
+            max(min(a.r, b.r), min(max(a.r, b.r), (1 - t) * a.r + t * b.r)),
+            max(min(a.g, b.g), min(max(a.g, b.g), (1 - t) * a.g + t * b.g)),
+            max(min(a.b, b.b), min(max(a.b, b.b), (1 - t) * a.b + t * b.b)),
+    };
+}
+
+float ilerp(float a, float b, float v)
+{
+    return (v - a) / (b - a);
+}
+
+class ColorGradient
+{
+public:
+    explicit ColorGradient(std::initializer_list<Color> colors...) :
+            colors{colors}
+    {
+        n = this->colors
+                .size() - 1;
+    }
+
+    Color get_color(float t) const
+    {
+        unsigned int i = get_index(t);
+        float dt = 1.f / n;
+        return lerp(colors[i], colors[i + 1], ilerp(i * dt, (i + 1) * dt, t));
+    }
+
+private:
+    unsigned int get_index(float t) const
+    {
+        float dt = 1.f / (float) n;
+        for (unsigned int i = 0; i < n; i++)
+        {
+            if (t <= (i + 1) * dt)
+            {
+                return i;
+            }
+        }
+        return n - 1;
+    }
+
+    std::vector<Color> colors;
+    unsigned int n;
+};
+
+//static ColorGradient colors{
+//        Color{0.0, 0.0, 0.0},
+//        Color{.14, .14, .77},
+//        Color{.46, .96, 1.0},
+//        Color{.93, .37, 1.0},
+//        Color{1.0, .91, 1.0}
+//};
+static ColorGradient colors{
+        Color{.00, .00, .00},
+        Color{.11, .67, .00},
+        Color{.61, 1.0, .25},
+        Color{1.0, 1.0, .37},
+        Color{.93, 1.0, .91}
+};
 
 /*
-// density check
-cout << grid._densities << endl;
-
-grid.addDensity(Tuple<int> (3, 3), 1.);
-grid.addDensity(Tuple<int> (0, 0), 0.);
-grid.addDensity(Tuple<int> (4, 4), 5.);
-
-cout << grid._densities << endl;
+  ----------------------------------------------------------------------
+   relates mouse movements to forces sources
+  ----------------------------------------------------------------------
 */
+
+static void get_from_UI(float *d, float *su, float *sv)
+{
+    int i, j, size = (N + 2) * (N + 2);
+
+    for (i = 0; i < size; i++)
+    {
+        su[i] = sv[i] = d[i] = 0.0f;
+    }
+
+    if (!mouse_down[0] && !mouse_down[2])
+    {
+        return;
+    }
+
+    i = static_cast<int>((static_cast<float>(mx) / static_cast<float>(win_x)) * Nf);
+    j = static_cast<int>((static_cast<float>(my) / static_cast<float>(win_y)) * Nf);
+
+    if (i < 1 || i > N || j < 1 || j > N)
+    {
+        return;
+    }
+
+    if (mouse_down[0])
+    {
+        su[IX(i, j)] = force * static_cast<float>(mx - omx);
+        sv[IX(i, j)] = force * static_cast<float>(my - omy);
+    }
+
+    if (mouse_down[2])
+    {
+        d[IX(i, j)] = source;
+    }
+
+    omx = mx;
+    omy = my;
+}
 
 /*
-// velocity check
-cout << grid._Vx << endl;
-
-grid.addVelocity(Tuple<int> (0, 0), Tuple<double> (3., 2.));
-grid.addVelocity(Tuple<int> (4, 4), Tuple<double> (0., 5.));
-
-cout << grid._Vx << endl;
-cout << grid._Vy << endl;
+  ----------------------------------------------------------------------
+   main --- main routine
+  ----------------------------------------------------------------------
 */
+
+const GLuint VERTEX_ATTR_POSITION = 0;
+
+void handleEvent(SDL_Event &e)
+{
+
+    if (e.type == SDL_MOUSEBUTTONDOWN)
+    {
+        omx = mx = e.button.x;
+        omy = my = e.button.y;
+        if (e.button.button == SDL_BUTTON_LEFT)
+        {
+            mouse_down[0] = 1;
+        }
+        if (e.button.button == SDL_BUTTON_RIGHT)
+        {
+            mouse_down[2] = 1;
+        }
+    }
+    else if (e.type == SDL_MOUSEBUTTONUP) {
+        if (e.button.button == SDL_BUTTON_LEFT)
+        {
+            mouse_down[0] = 0;
+        }
+        if (e.button.button == SDL_BUTTON_RIGHT)
+        {
+            mouse_down[2] = 0;
+        }
+    }
+    else if (e.type == SDL_MOUSEMOTION)
+    {
+        mx = e.motion.x;
+        my = e.motion.y;
+    }
+}
+
+void print_array(float *array)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            std::cout << array[IX(i, j)] << "|";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    N = 128;
+    Ts = 5;
+    Nf = static_cast<float>(N);
+    dt = 0.1f;
+    diff = 0.0f;
+    visc = 0.0001f;
+    force = 5.0f;
+    source = 100.0f;
+    win_x = N * Ts;
+    win_y = N * Ts;
+
+    if (!allocate_data())
+    {
+        exit(1);
+    }
+    clear_data();
+
+    SDLWindowManager windowManager(win_x, win_y, "UGE");
+    GLenum glewInitError = glewInit();
+    if (GLEW_OK != glewInitError)
+    {
+        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        return EXIT_FAILURE;
+    }
+    FilePath applicationPath(argv[0]);
+    Program program = loadProgram(applicationPath.dirPath() + "shaders/grid.vs.glsl",
+                                  applicationPath.dirPath() + "shaders/grid.fs.glsl");
+    program.use();
+
+    /*********************************
+     * HERE SHOULD COME THE INITIALIZATION CODE
+     *********************************/
+
+    GLuint buff_id;
+    glGenBuffers(1, &buff_id);
+    glBindBuffer(GL_ARRAY_BUFFER, buff_id);
+    float dx = 2.f * (float) Ts / (float) win_x;
+    float dy = 2.f * (float) Ts / (float) win_y;
+    GLfloat vertices[] = {
+            -1.f, 1.f, -1.f + dx, 1.f, -1.f + dx, 1.f - dy,
+            -1.f, 1.f, -1.f + dx, 1.f - dy, -1.f, 1.f - dy
+    };
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buff_id);
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    GLuint uColor = glGetUniformLocation(program.getGLId(), "uColor");
+    GLuint uTranslation = glGetUniformLocation(program.getGLId(), "uTranslation");
+
+    // Application loop:
+    bool done = false;
+    while (!done)
+    {
+        // Event loop:
+        SDL_Event e;
+        while (windowManager.pollEvent(e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                done = true; // Leave the loop after this iteration
+            }
+            if (e.type == SDL_KEYDOWN)
+            {
+                if (e.key.keysym.sym == SDLK_a)
+                {
+                    print_array(dens);
+                }
+                if (e.key.keysym.sym == SDLK_c)
+                {
+                    clear_data();
+                }
+                if (e.key.keysym.sym == SDLK_v) {
+                    visc += 0.0001f;
+                }
+            }
+            handleEvent(e);
+        }
+        get_from_UI(dens_prev, u_prev, v_prev);
+
+        vel_step(N, u, v, u_prev, v_prev, visc, dt);
+        dens_step(N, dens, dens_prev, u, v, diff, dt);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBindVertexArray(vao);
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                auto src = dens[IX(i, j)];
+                if (src < 0.001)
+                {
+                    continue;
+                }
+                Color c = colors.get_color(src);
+                glUniform3f(uColor, c.r, c.g, c.b);
+                glUniform2f(uTranslation, 2 * (float) (i * Ts) / (float) win_x, -2 * (float) (j * Ts) / (float) win_y);
+                glDrawArrays(GL_TRIANGLES, 0, 6);
+            }
+        }
+
+//        glUniform3f(uColor, 0, 0, 1);
+//        glUniform2f(uTranslation, 0,0);
+//        glDrawArrays(GL_TRIANGLES, 0, 6);
+//        glBindVertexArray(0);
+
+        /*********************************
+         * HERE SHOULD COME THE RENDERING CODE
+         *********************************/
+
+        // Update the display
+        windowManager.swapBuffers();
+    }
+
+    free_data();
+    glDeleteBuffers(1, &buff_id);
+    glDeleteVertexArrays(1, &vao);
+
+    return EXIT_SUCCESS;
+}
--- src/FluidGrid2.cpp
+++ src/FluidGrid2.cpp
@@ -1,136 +1,165 @@
-#include <FluidGrid2.hpp>
+#define IX(i, j) ((i)+(N+2)*(j))
+#define SWAP(x0, x) {float * tmp=x0;x0=x;x=tmp;}
+#define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) {
+#define END_FOR }}
 
-#include <cmath>
-#include <algorithm>
-
-
-
-void FluidGrid2::add_density(Tuple<int> coords, double amount)
+void add_source(int N, float *x, const float *s, float dt)
 {
-    _sources[coords.x][coords.y] += amount;
-}
-
-void FluidGrid2::add_source()
-{
-    for (int i = 0; i < _size; i++)
+    int i, size = (N + 2) * (N + 2);
+    for (i = 0; i < size; i++)
     {
-        for (int j = 0; j < _size; j++)
-        {
-            _densities[i][j] += _lifespan * _sources[i][j];
-        }
+        x[i] += dt * s[i];
     }
 }
 
-
-void FluidGrid2::set_bnd(int bound, Grid& x)
+void set_bnd(int N, int b, float *x)
 {
-    //std::cout << x;
-    //std::cout << x[0][2] << std::endl;
-    for (int i = 1; i <= side(); ++i)
-    {
-        x[0       ][i] = bound == 1 ? -x[1     ][i] : x[1     ][i];
-        x[side()+1][i] = bound == 1 ? -x[side()][i] : x[side()][i];
-        x[i][0       ] = bound == 2 ? -x[i][1     ] : x[i][1     ];
-        x[i][side()+1] = bound == 2 ? -x[i][side()] : x[i][side()];
-    }
+    int i;
 
-    x[0       ][0       ] = .5 * (x[1     ][0       ] + x[0       ][1     ]);
-    x[0       ][side()+1] = .5 * (x[1     ][side()+1] + x[0       ][side()]);
-    x[side()+1][0       ] = .5 * (x[side()][0       ] + x[side()+1][1     ]);
-    x[side()+1][side()+1] = .5 * (x[side()][side()+1] + x[side()+1][side()]);
+    for (i = 1; i <= N; i++)
+    {
+        /* periodic bc */
+        x[IX(0  ,i)] = b==1 ? -x[IX(1,i)] : x[IX(1,i)];
+        x[IX(N+1,i)] = b==1 ? -x[IX(N,i)] : x[IX(N,i)];
+        x[IX(i,0  )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
+        x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];
+        /* lid driven cavity bc */
+        // left
+//        x[IX(0, i)] = 0.f;
+//        // right
+//        x[IX(N + 1, i)] = 0.f;
+//        // bottom
+//        x[IX(i, 0)] = 0.f;
+//        // top / lid
+//        x[IX(i, N + 1)] = 0;
+    }
+    x[IX(0, 0)] = 0.5f * (x[IX(1, 0)] + x[IX(0, 1)]);
+    x[IX(0, N + 1)] = 0.5f * (x[IX(1, N + 1)] + x[IX(0, N)]);
+    x[IX(N + 1, 0)] = 0.5f * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
+    x[IX(N + 1, N + 1)] = 0.5f * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
 }
 
-
-void FluidGrid2::diffuse(int bound, Grid& x, const Grid& x0, int iter)
+void lin_solve(int N, int b, float *x, const float *x0, float a, float c)
 {
-    double a = _lifespan * _diffusion * in_cells_n();
+    int i, j, k;
 
-    for (int k = 0; k < iter; k++ ) 
+    for (k = 0; k < 20; k++)
     {
-        for (int i = 1; i <= side(); i++ ) 
-        {
-            for (int j = 1 ; j <= side(); j++ ) 
+        FOR_EACH_CELL
+                x[IX(i, j)] =
+                        (x0[IX(i, j)] + a * (x[IX(i - 1, j)] + x[IX(i + 1, j)] + x[IX(i, j - 1)] + x[IX(i, j + 1)]))
+                        / c;
+        END_FOR
+        set_bnd(N, b, x);
+    }
+}
+
+void diffuse(int N, int b, float *x, float *x0, float diff, float dt)
+{
+    auto Nf = static_cast<float>(N);
+    float a = dt * diff * Nf * Nf;
+    lin_solve(N, b, x, x0, a, 1 + 4 * a);
+}
+
+/*
+*
+*	->Advect step:
+*
+*		*d : pointer to advected scalar
+*		*u : pointer to advective velocity field (x-component)
+*		*v : pointer to advective velocity field (y-component)
+*		 b = 0 : density advection
+*		   = 1 : u-component advection
+*		   = 2 : v-component advection
+*
+*/
+void advect(int N, int b, float *d, const float *d0, const float *u, const float *v, float dt)
+{
+    auto Nf = static_cast<float>(N);
+    int i, j, i0, j0, i1, j1;
+    float x, y, s0, t0, s1, t1, dt0;
+
+    dt0 = dt * Nf;
+    FOR_EACH_CELL
+            // fluid particle back tracing
+            x = static_cast<float>(i) - dt0 * u[IX(i, j)];
+            y = static_cast<float>(j) - dt0 * v[IX(i, j)];
+            // find nodes adjacent to former position
+            if (x < 0.5f)
             {
-                auto tmp = x0[i-1][j] + x0[i+1][j] + x0[i][j-1] + x0[i][j+1];
-                x[i][j]  = (x0[i][j] + a*tmp) / (1+4*a);
+                x = 0.5f;
             }
-        }
-        set_bnd (bound, x);
-    }
+            if (x > Nf + 0.5f)
+            {
+                x = Nf + 0.5f;
+            }
+            i0 = (int) x;
+            i1 = i0 + 1;
+            if (y < 0.5f)
+            {
+                y = 0.5f;
+            }
+            if (y > Nf + 0.5f)
+            {
+                y = Nf + 0.5f;
+            }
+            j0 = (int) y;
+            j1 = j0 + 1;
+            // bi-linear interpolation from adjacent nodes to former position x
+            s1 = x - static_cast<float>(i0);
+            s0 = 1 - s1;
+            t1 = y - static_cast<float>(j0);
+            t0 = 1 - t1;
+            d[IX(i, j)] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)]) +
+                          s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);
+    END_FOR
+    set_bnd(N, b, d);
 }
 
-
-void FluidGrid2::advect(int bound, Grid& d, const Grid& d0, const Tuple<Grid>& velocities)
+void project(int N, float *u, float *v, float *p, float *div)
 {
-    Grid u = velocities.x;
-    Grid v = velocities.y;
-    double ls0 = _lifespan * side();
-    for (int i = 1; i <= side(); i++)
-    {
-        for (int j = 1; j <= side(); j++)
-        {
-            double x = i - ls0 * u[i][j];
-            double y = j - ls0 * v[i][j];
+    auto Nf = static_cast<float>(N);
+    int i, j;
 
-            if (x < .5) x = .5;
-            if (x > side()+.5) x = side()+.5;
-            int i0 = x;
-            int i1 = i0 + 1;
+    FOR_EACH_CELL
+            div[IX(i, j)] = -0.5f * (u[IX(i + 1, j)] - u[IX(i - 1, j)] + v[IX(i, j + 1)] - v[IX(i, j - 1)]) / Nf;
+            p[IX(i, j)] = 0;
+    END_FOR
+    set_bnd(N, 0, div);
+    set_bnd(N, 0, p);
 
-            if (y < .5) y = .5;
-            if (y > side()+.5) y = side()+.5;
-            int j0 = y;
-            int j1 = j0 + 1;
+    lin_solve(N, 0, p, div, 1, 4);
 
-            double s1 = x - i0;
-            double s0 = 1 - s1;
-            double t1 = y - j0;
-            double t0 = 1 - t1;
-
-            d[i][j] = s0 * (t0 * d0[i0][j0] + t1 * d0[i0][j1]) + 
-                    + s1 * (t0 * d0[i1][j0] + t1 * d0[i1][j1]);
-        }
-    }
-    set_bnd(bound, d);
+    FOR_EACH_CELL
+            u[IX(i, j)] -= 0.5f * Nf * (p[IX(i + 1, j)] - p[IX(i - 1, j)]);
+            v[IX(i, j)] -= 0.5f * Nf * (p[IX(i, j + 1)] - p[IX(i, j - 1)]);
+    END_FOR
+    set_bnd(N, 1, u);
+    set_bnd(N, 2, v);
 }
 
-
-void FluidGrid2::dens_step(const Tuple<Grid>& vel)
+void dens_step(int N, float *x, float *x0, float *u, float *v, float diff, float dt)
 {
-    add_source();
-
-    std::swap(_sources, _densities);
-    diffuse(0, _densities, _sources, 20);
-
-    std::swap(_sources, _densities);
-    advect(0, _densities, _sources, vel);
+    add_source(N, x, x0, dt);
+    SWAP (x0, x)
+    diffuse(N, 0, x, x0, diff, dt);
+    SWAP (x0, x)
+    advect(N, 0, x, x0, u, v, dt);
 }
 
-std::ostream& operator<<(std::ostream& os, const Grid& grid)
+void vel_step(int N, float *u, float *v, float *u0, float *v0, float viscosity, float dt)
 {
-    os << "--- GRID --- " << std::endl;
-    for (auto line : grid)
-    {
-        for (auto cell : line)
-        {
-            os << cell << " ";
-            //os << ceilf(cell) << "          ";
-        }
-        os << std::endl;
-    }
-    return os;
+    add_source(N, u, u0, dt);
+    add_source(N, v, v0, dt);
+    SWAP (u0, u)
+    diffuse(N, 1, u, u0, viscosity, dt);
+    SWAP (v0, v)
+    diffuse(N, 2, v, v0, viscosity, dt);
+    project(N, u, v, u0, v0);
+    SWAP (u0, u)
+    SWAP (v0, v)
+    advect(N, 1, u, u0, u0, v0, dt);
+    advect(N, 2, v, v0, u0, v0, dt);
+    project(N, u, v, u0, v0);
 }
 
-
-std::ostream& operator<<(std::ostream& os, FluidGrid2& fc)
-{
-    os << "=== FLUIDGRID === " << std::endl;
-    os << "side length : " << fc._size << " | ";
-    os << "side without borders : " << fc.side() << " | ";
-    os << "nb cells : " << fc.cells_n() << " | ";
-    os << "diffusion : " << fc._diffusion << " | ";
-    //os << "viscosity : " << fc._viscosity << " | ";
-    os << "lifespan : " << fc._lifespan << std::endl;
-    //os << fc._s;
-    return os;
-}
